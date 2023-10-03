8461a16db7337db9e52f507a00eb8b73009bdfb2
Alex Chan <alex@alexwlchan.net>
2023-10-03 10:21:26 +0100

    Move the fixture out into conftest.py

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import os
+
+import pytest
+import vcr
+
+from flinumeratr.flickr_api import FlickrApi
+
+
+@pytest.fixture(scope='function')
+def api(request):
+    """
+    Creates an instance of the FlickrApi class for use in tests.
+    
+    This instance of the API will record its interactions as "cassettes"
+    using vcr.py, which can be replayed offline (e.g. in CI tests).
+    """
+    with vcr.use_cassette(
+        request.function.__name__ + '.yml',
+        cassette_library_dir='tests/fixtures/cassettes',
+        filter_query_parameters=['api_key'],
+        record_mode='once',
+    ):
+        yield FlickrApi(api_key=os.environ.get('FLICKR_API_KEY', '<REDACTED>'))
--- tests/test_flickr_api.py
+++ tests/test_flickr_api.py
@@ -1,28 +1,8 @@
 import datetime
-import os
 
 import httpx
-import pytest
-import vcr
 
-from flinumeratr.flickr_api import FlickrApi, get_single_photo_info
-
-
-@pytest.fixture(scope='function')
-def api(request):
-    """
-    Creates an instance of the FlickrApi class for use in tests.
-    
-    This instance of the API will record its interactions as "cassettes"
-    using vcr.py, which can be replayed offline (e.g. in CI tests).
-    """
-    with vcr.use_cassette(
-        request.function.__name__ + '.yml',
-        cassette_library_dir='tests/fixtures/cassettes',
-        filter_query_parameters=['api_key'],
-        record_mode='once',
-    ):
-        yield FlickrApi(api_key=os.environ.get('FLICKR_API_KEY', '<REDACTED>'))
+from flinumeratr.flickr_api import get_single_photo_info
 
 
 def test_get_single_photo_info(api):
