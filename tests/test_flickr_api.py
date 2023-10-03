d52571a334750b3631e622964ea350aed515aa9e
Alex Chan <alex@alexwlchan.net>
2023-10-03 10:16:36 +0100

    Pull this into a fixture

--- tests/test_flickr_api.py
+++ tests/test_flickr_api.py
@@ -1,23 +1,24 @@
 import os
 
 import httpx
+import pytest
 import vcr
 
 from flinumeratr.flickr_api import FlickrApi, get_single_photo_info
 
 
-@vcr.use_cassette(
-    cassette_library_dir='tests/fixtures/cassettes',
-    filter_query_parameters=['api_key'],
-    record_mode='once',
-)
-def test_get_single_photo_info():
-    api = FlickrApi(api_key=os.environ.get('FLICKR_API_KEY', '<REDACTED>'))
-    client = httpx.Client(
-    base_url=api.client.base_url,
-    params=api.client.params)
-    api.client = client
-    
+@pytest.fixture(scope='function')
+def api(request):
+    with vcr.use_cassette(
+        request.function.__name__ + '.yml',
+        cassette_library_dir='tests/fixtures/cassettes',
+        filter_query_parameters=['api_key'],
+        record_mode='once',
+    ):
+        yield FlickrApi(api_key=os.environ.get('FLICKR_API_KEY', '<REDACTED>'))
+
+
+def test_get_single_photo_info(api):
     info = get_single_photo_info(api, photo_id='32812033543')
     assert info == {
         'title': 'Puppy Kisses'
