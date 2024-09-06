1057f9783b456e06a4b9b4b7d6ba0918147745df
Alex Chan <alex@alexwlchan.net>
2024-09-06 10:44:39 +0100

    Add config for `interrogate`
    It's not much, but it establishes a baseline we can start to crank up
    from.  For https://github.com/Flickr-Foundation/infrastructure/issues/42

--- glitch/redirectr.py
+++ glitch/redirectr.py
@@ -1,3 +1,7 @@
+"""
+A tiny Flask app that forwards all requests to www.flickr.org.
+"""
+
 from flask import Flask, Response, redirect, request
 
 app = Flask(__name__)
@@ -6,6 +10,9 @@ app = Flask(__name__)
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def index(path: str) -> Response:
+    """
+    Redirect any path and query string to Flinumeratr running on flickr.org.
+    """
     if request.query_string:
         return redirect(
             "https://www.flickr.org/tools/flinumeratr/"
